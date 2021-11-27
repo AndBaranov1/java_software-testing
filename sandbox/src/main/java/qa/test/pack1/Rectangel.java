131fcc32ba78e62611551ffba564c673571029cd
AndBaranov1 <37299544+AndBaranov1@users.noreply.github.com>
2021-11-27 18:55:13 +0700

    лекции 1.7-1.9

---
+++ sandbox/src/main/java/qa/test/pack1/Rectangel.java
@@ -0,0 +1,16 @@
+package qa.test.pack1;
+
+public class Rectangel {
+
+  public double a;
+  public double b;
+
+  public Rectangel(double a, double b) {
+    this.a = a;
+    this.b = b;
+  }
+
+  public double area() {
+    return this.a * this.b;
+  }
+}
