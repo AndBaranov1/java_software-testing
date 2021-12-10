4387a4216265d5c595ea5f914c1b258c278f9ad9
AndBaranov1 <37299544+AndBaranov1@users.noreply.github.com>
2021-12-10 17:15:03 +0700

    Созданы тесты для контактов в адресной книге

---
+++ addressbook-web-test/src/test/java/qa/test/addressbook/ContactData.java
@@ -0,0 +1,5 @@
+package qa.test.addressbook;
+
+public record ContactData(String middlename, String lastname, String nickname, String title, String company,
+                          String address, String fname) {
+}
