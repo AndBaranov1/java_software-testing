ed8ac0217919eea9b4ec279daecebf58a4095ab5
AndBaranov1 <37299544+AndBaranov1@users.noreply.github.com>
2022-01-19 01:18:17 +0700

    Автоматическое закрытие файлов после использования

--- addressbook-web-test/src/test/java/qa/test/addressbook/generators/ContactDataGenerator.java
+++ addressbook-web-test/src/test/java/qa/test/addressbook/generators/ContactDataGenerator.java
@@ -40,19 +40,19 @@ public class ContactDataGenerator {
   }
   private void saveASJson(List<ContactData> contacts, File file) throws IOException {
     Gson gson = new GsonBuilder().setPrettyPrinting().excludeFieldsWithoutExposeAnnotation().create();
-    String json =gson.toJson(contacts);
-    Writer writer = new FileWriter(file);
-    writer.write(json);
-    writer.close();
+    String json = gson.toJson(contacts);
+    try (Writer writer = new FileWriter(file)) {
+      writer.write(json);
+    }
   }
 
   private void saveAsXml(List<ContactData> contacts, File file) throws IOException {
     XStream xstream = new XStream();
     xstream.processAnnotations(ContactData.class);
     String xml = xstream.toXML(contacts);
-    Writer writer = new FileWriter(file);
-    writer.write(xml);
-    writer.close();
+    try (Writer writer = new FileWriter(file)) {
+      writer.write(xml);
+    }
   }
 
   public static void main(String[] args) throws IOException {
@@ -69,12 +69,12 @@ public class ContactDataGenerator {
 
   private void saveAsCsv(List<ContactData> contacts, File file) throws IOException {
     System.out.println(new File(".").getAbsolutePath());
-    Writer writer = new FileWriter(file);
-    for (ContactData contact : contacts) {
-      writer.write(String.format("%s;%s;%s;%s;%s;%s;%s\n", contact.getMiddlename(), contact.getAddress(), contact.getLastname(), contact.getPhoneHome(),
-              contact.getNickname(), contact.getPhoneMobile(), contact.getEmail()));
+    try (Writer writer = new FileWriter(file)) {
+      for (ContactData contact : contacts) {
+        writer.write(String.format("%s;%s;%s;%s;%s;%s;%s\n", contact.getMiddlename(), contact.getAddress(), contact.getLastname(), contact.getPhoneHome(),
+                contact.getNickname(), contact.getPhoneMobile(), contact.getEmail()));
+      }
     }
-    writer.close();
   }
 
   private List<ContactData> generateContact(int count) {
