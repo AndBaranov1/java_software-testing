a5ddb82ca1555634801b73c419ea6ded93a55181
AndBaranov1 <37299544+AndBaranov1@users.noreply.github.com>
2022-01-31 12:58:18 +0700

    Исправление 9.2

--- soap-sample/src/test/java/GeoIpServiceTests.java
+++ soap-sample/src/test/java/GeoIpServiceTests.java
@@ -10,7 +10,7 @@ public class GeoIpServiceTests {
     assertEquals(geoIP, "<GeoIP><Country>RU</Country><State>54</State></GeoIP>");
   }
 
-  @Test(enabled = true)
+  @Test
   public void testInvalidIp() {
     String geoIP = new GeoIPService().getGeoIPServiceSoap12().getIpLocation20("188.162.86.x");
     assertEquals(geoIP, "<GeoIP><Country>RU</Country><State>54</State></GeoIP>");
