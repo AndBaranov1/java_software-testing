f61183f449c0292dba65f2809feb5a0d560f7974
AndBaranov1 <37299544+AndBaranov1@users.noreply.github.com>
2022-02-04 16:08:11 +0700

    Изменения для 10.2

--- addressbook-web-test/src/test/java/qa/test/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-test/src/test/java/qa/test/addressbook/tests/GroupCreationTests.java
@@ -47,6 +47,7 @@ public class GroupCreationTests extends TestBase {
         json += line;
         line = reader.readLine();
       }
+      //Gson gson =new Gson();
       Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
       List<GroupData> groups = gson.fromJson(json, new TypeToken<List<GroupData>>(){}.getType());
       return groups.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
@@ -57,10 +58,10 @@ public class GroupCreationTests extends TestBase {
   @Test(dataProvider = "validGroupsFromJson")
   public void testGroupCreation(GroupData group)  {
       app.goTo().groupPage();
-      Groups before = app.group().all();
+      Groups before = app.db().groups();
       app.group().create(group);
       assertThat(app.group().count(), equalTo(before.size() + 1));
-      Groups after = app.group().all();
+      Groups after = app.db().groups();
       assertThat(after, equalTo(
               before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
 
@@ -69,11 +70,12 @@ public class GroupCreationTests extends TestBase {
   @Test(enabled = false)
   public void testBadGroupCreation() throws Exception {
     app.goTo().groupPage();
-    Groups before = app.group().all();
-    GroupData group = new GroupData().withName("test_group9");
+    Groups before = app.db().groups();
+    GroupData group = new GroupData().withName("test'");
     app.group().create(group);
-    assertThat(app.group().count(), equalTo(before.size() + 1));
-    Groups after = app.group().all();
+    assertThat(app.group().count(), equalTo(before.size()));
+    Groups after = app.db().groups();
     assertThat(after, equalTo(before));
+    verifyGroupListInUI();;
   }
 }
